16cd83530c28f525398eeb1e3034cdc954330324
Omer Degirmenci <omer.degirmenci@system4.nl>
2022-11-20 21:39:39 +0100

    build

--- shopify-storefront/src/components/Hero.js
+++ shopify-storefront/src/components/Hero.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import { Box, Button, Center, Image, Text } from '@chakra-ui/react'
+import React from "react";
+import { Box, Button, Center, Image, Text } from "@chakra-ui/react";
 
 const Hero = () => {
   return (
-    <Box backgroundColor='#FFA8E2' w='100%' position='relative' h='74vh'>
+    <Box backgroundColor="#FFA8E2" w="100%" position="relative" h="74vh">
       <Image
-        className='fade-in'
-        h='100%'
-        m='auto'
-        objectFit='contain'
-        objectPosition={['top', 'center']}
-        src='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Bath_Bomb_-_Product-4_-_nobg_1.png?v=1610055851.jpg'
+        className="fade-in"
+        h="100%"
+        m="auto"
+        objectFit="contain"
+        objectPosition={["top", "center"]}
+        src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Bath_Bomb_-_Product-4_-_nobg_1.png?v=1610055851.jpg"
       />
       <Text
-        className='tracking-in-expand-fwd'
-        position='absolute'
-        bottom='10%'
-        w='100%'
-        textAlign='center'
-        color='white'
-        fontWeight='bold'
-        fontSize='3.5rem'
+        className="tracking-in-expand-fwd"
+        position="absolute"
+        bottom="10%"
+        w="100%"
+        textAlign="center"
+        color="white"
+        fontWeight="bold"
+        fontSize="3.5rem"
       >
-        Discover Bath Bombs
+        Ontdek Rumi's Keuken!
       </Text>
       <Center>
         <Button
-          w='10rem'
-          backgroundColor='#FF38BD'
-          color='white'
-          _hover={{ opacity: '70%' }}
-          position='absolute'
-          bottom='0%'
+          w="10rem"
+          backgroundColor="#FF38BD"
+          color="white"
+          _hover={{ opacity: "70%" }}
+          position="absolute"
+          bottom="0%"
         >
-          Shop Now
+          Zie onze gerechten
         </Button>
       </Center>
     </Box>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- shopify-storefront/src/components/Navbar.js
+++ shopify-storefront/src/components/Navbar.js
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import { ShopContext } from "../context/shopContext";
 import { MdMenu, MdShoppingBasket } from "react-icons/md";
 import { Link } from "react-router-dom";
+//src={`${process.env.PUBLIC_URL}/images/cig_kofte.jpeg`}
 
 const Navbar = () => {
   const { openCart, openMenu, checkout } = useContext(ShopContext);
@@ -25,7 +26,9 @@ const Navbar = () => {
 
       <Link to={"/"}>
         <Image
-          src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540"
+          src={
+            "https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540"
+          }
           w={100}
           h={100}
         />
